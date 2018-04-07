4f5f87692f3ddf608723f41018fed5ac8caee7df
Chris <christof.rescher@gmail.com>
2018-04-07 18:40:53 +0200

    eee

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as Pull from 'pulljs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  pull = Pull.init({
+              instructionsPullToRefresh: 'Pull to Refresh',
+              instructionsReleaseToRefresh: 'Loslassen um zu Aktualisieren',
+              instructionsRefreshing: 'Aktualsiere...',
+              onRefresh: () => alert('hello pulljs')
+  });
+
+
 }
